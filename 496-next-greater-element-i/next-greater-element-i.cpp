7987457ad758e17ab5c92734114dba56e3c820c9
Prathamesh Gangawane <prash6130@gmail.com>
2026-09-22 19:47:46 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (14.74%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    int getGreater(int value,vector<int> &nums2){
-           for(int i=0; i<nums2.size()-1; i++){
-              if(nums2[i]==value){
-                 for(int j=i+1; j<nums2.size(); j++){
-                    if(nums2[j]>value)return nums2[j];
-                 }     
-              }
-           }
-           return -1;
-    }
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans;
-        for(int i=0; i<nums1.size(); i++){
-            int x=getGreater(nums1[i],nums2);
-            ans.push_back(x);
+    vector<int>nextgreater(vector<int>&nums2){
+        stack<int>st;
+        vector<int>ans(nums2.size(), 0);
+
+        for(int i=nums2.size()-1; i>=0; i--){
+            while(st.size()>0 && st.top()<=nums2[i])st.pop();
+            if(st.empty())ans[i]=-1;
+            else ans[i]=st.top();
+            st.push(nums2[i]);
         }
         return ans;
+    }
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+         vector<int>nextgreatervala=nextgreater(nums2);
+         unordered_map<int,int>mpp;
+         for(int i=0; i<nextgreatervala.size(); i++){
+            mpp[nums2[i]]=nextgreatervala[i];
+         }
+
+         vector<int>ans;
+         for(int i=0; i<nums1.size(); i++){
+            ans.push_back(mpp[nums1[i]]);
+         }
+         return ans;
     }
 };
